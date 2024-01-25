8b624dc3fe908c8f817cb5869238c8ec1269575d
Petru Sabadac <petru.sabadac@outlook.com>
2024-01-25 21:43:10 +0200

    Drafted WeatherRow

---
+++ app/src/main/java/com/sabadac/isimosezulu/ui/weather_screen/WeatherRow.kt
@@ -0,0 +1,65 @@
+package com.sabadac.isimosezulu.ui.weather_screen
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import com.sabadac.isimosezulu.R
+
+@Composable
+fun WeatherRow(
+    weather: Weather,
+    modifier: Modifier
+) {
+    Row(verticalAlignment = Alignment.CenterVertically,
+        modifier = modifier.padding(top = 4.dp, bottom = 4.dp)) {
+
+        Column(modifier = Modifier
+            .weight(1f)
+            .padding(start = 16.dp)) {
+            Text(
+                text = weather.min,
+                color = Color.White
+            )
+            Text(
+                text = stringResource(id = R.string.min).lowercase(),
+                color = Color.White
+            )
+        }
+
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            modifier = Modifier.weight(1f)) {
+            Text(
+                text = weather.current,
+                color = Color.White
+            )
+            Text(
+                text = stringResource(id = R.string.current).lowercase(),
+                color = Color.White
+            )
+        }
+
+        Column(
+            horizontalAlignment = Alignment.End,
+            modifier = Modifier
+                .weight(1f)
+                .padding(end = 16.dp)) {
+            Text(
+                text = weather.max,
+                color = Color.White,
+                modifier = Modifier.padding(end = 8.dp)
+            )
+            Text(
+                text = stringResource(id = R.string.max).lowercase(),
+                color = Color.White
+            )
+        }
+    }
+}
--- app/src/main/java/com/sabadac/isimosezulu/ui/weather_screen/WeatherSection.kt
+++ app/src/main/java/com/sabadac/isimosezulu/ui/weather_screen/WeatherSection.kt
@@ -1,8 +1,11 @@
 package com.sabadac.isimosezulu.ui.weather_screen
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Divider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun WeatherSection(
@@ -10,7 +13,15 @@ fun WeatherSection(
     modifier: Modifier
 ) {
     Column(modifier = modifier) {
-        WeatherField(weather = weather, modifier = Modifier)
+        WeatherField(
+            weather = weather,
+            modifier = Modifier
+        )
+        WeatherRow(
+            weather = weather,
+            modifier = Modifier
+        )
+        Divider(color = Color.White, thickness = 1.dp)
     }
 }
 
